e6d34e1a6464ef09115e258b502dd614f04b6c7b
Khoa Pham <kenjifam@gmail.com>
2020-03-14 23:15:56 +0200

    theme and global styles

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,28 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          margin: 0,
+          fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+                'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
+          '-webkit-font-smoothing': 'antialiased',
+          '-moz-osx-font-smoothing': 'grayscale',
+          fontSize: 16,
+          backgroundColor: '#fcfcfc',
+        },
+        '*': {
+          boxSizing: 'border-box',
+        },
+      },
+    },
+  },
+  palette: {
+    primary: blue,
+  },
+});
+
+export default theme;
