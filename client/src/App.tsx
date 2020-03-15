9c90a5a1a12881dd7852f3d20833ca3114840f2e
Khoa Pham <kenjifam@gmail.com>
2020-03-15 12:49:38 +0200

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Layout from './views/Layout';
+import Home from './views/Home';
 
 const App = () => {
   return (
-    <div className="App">
-      <Layout>Hello</Layout>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/partners" component={Home} />
+          <Route path="/requests" component={Home} />
+          <Route path="/about" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Redirect to="/" />
+        </Switch>
+      </Layout>
+    </Router>
   );
 };
 
