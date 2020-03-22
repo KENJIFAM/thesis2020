a2b3adddb60e1512927255e924fc56ff4eadd65b
Khoa Pham <kenjifam@gmail.com>
2020-03-22 09:59:41 +0200

    fix port server

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ app.get('/*', (req, res) => {
   res.send('Welcome to Food Help APIs!');
 });
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server is running at port 4001');
+  console.log('Server is running at port 5000');
 });
