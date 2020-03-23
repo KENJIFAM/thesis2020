48e66bafc42669a0c3e59394d85e851a8de53e3e
Khoa Pham <kenjifam@gmail.com>
2020-03-23 20:15:21 +0200

    fix jwt not return

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -5,17 +5,19 @@ const router = express.Router();
 
 router.post('/login', async (req, res, next) => {
   try {
-    const user = await db.User.findOne({
-      email: req.body.email,
-    });
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next({ status: 400, message: 'Missing email or password!' });
+    }
+    const user = await db.User.findOne({ email });
     if (!user) {
       return next({ status: 401, message: 'No user existed with this email!' });
     }
-    const isMatch = await user.validatePassword(req.body.password, next);
+    const isMatch = await user.validatePassword(password, next);
     if (!isMatch) {
       return next({ status: 401, message: 'Incorrect password!' });
     }
-    const token = user.generateJwt(next);
+    const token = await user.generateJwt(next);
     return res.status(200).json({ token });
   } catch (err) {
     return next(err);
@@ -25,7 +27,7 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await db.User.create(req.body);
-    const token = user.generateJwt(next);
+    const token = await user.generateJwt(next);
     return res.status(200).json({ token });
   } catch (err) {
     if (err.code === 11000) {
