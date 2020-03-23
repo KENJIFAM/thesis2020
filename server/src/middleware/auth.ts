807e7bfa5fb40c798378aa724068174c7826f77f
Khoa Pham <kenjifam@gmail.com>
2020-03-23 21:44:57 +0200

    create checklogin and checkcorrectuser middleware

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { JwtPayload } from '../models/User';
+
+//Authentication
+export const checkLogIn = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1] || '';
+    jwt.verify(token, process.env.SECRET_KEY as string, (err, decoded) =>
+      decoded ? next() : next({ status: 401, message: 'Please log in first!' }),
+    );
+  } catch (err) {
+    return next({ status: 401, message: 'Please log in first!' });
+  }
+};
+
+//Authorization
+export const checkCorrectUser = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1] || '';
+    jwt.verify(token, process.env.SECRET_KEY as string, (err, decoded) =>
+      (decoded as JwtPayload)?.id === req.params.id
+        ? next()
+        : next({ status: 401, message: 'Unauthorized!' }),
+    );
+  } catch (err) {
+    return next({ status: 401, message: 'Unauthorized!' });
+  }
+};
