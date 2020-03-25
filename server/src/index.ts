ca399ad055db36139ae72a5dad1054f545eef7c0
Khoa Pham <kenjifam@gmail.com>
2020-03-25 21:05:38 +0200

    reorder express middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,19 @@ import cors from 'cors';
 import compression from 'compression';
 import helmet from 'helmet';
 import authController from './controllers/authController';
+import userController from './controllers/userController';
 import errorHandler from './middleware/error';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
 app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/auth', authController);
+app.use('/profile/', userController);
 
 app.get('/*', (req, res) => {
   res.send('Welcome to Food Help APIs!');
@@ -23,5 +25,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server is running at port 5000');
+  console.log(`Server is running at port ${PORT}`);
 });
