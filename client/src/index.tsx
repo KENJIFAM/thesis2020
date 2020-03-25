dcae475ac13d9ae19c2839248333baa229fe6660
Khoa Pham <kenjifam@gmail.com>
2020-03-26 01:19:54 +0200

    add MHR to index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './styles/theme';
-import ReduxRoot from './ReduxRoot';
+import store from './store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -14,12 +15,19 @@ const AppWithStyles = () => (
   </ThemeProvider>
 );
 
-ReactDOM.render(
-  <ReduxRoot>
-    <AppWithStyles />
-  </ReduxRoot>,
-  document.getElementById('root'),
-);
+const render = () =>
+  ReactDOM.render(
+    <Provider store={store}>
+      <AppWithStyles />
+    </Provider>,
+    document.getElementById('root'),
+  );
+
+render();
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./App', render);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
