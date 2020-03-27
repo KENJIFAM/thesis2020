2df4b69c74a7b23edb62e1a6ace5e75b72317bfb
Khoa Pham <kenjifam@gmail.com>
2020-03-27 21:45:36 +0200

    replace auth middleware with express-jwt

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-import { checkLogIn, checkCorrectUser } from '../middleware/auth';
+import auth, { AuthRequest } from '../middleware/auth';
 import db, { UserModel } from '../models';
 
 const router = express.Router();
 
-router.get('/', checkLogIn, checkCorrectUser, async (req, res, next) => {
+router.get('/', auth, async (req: AuthRequest, res, next) => {
   try {
-    const user = (await db.User.findById(req.body.id).orFail()) as UserModel;
+    if (!req.auth?.id) {
+      return next({ status: 401, message: 'Unauthorized error: private profile' });
+    }
+    const user = (await db.User.findById(req.auth.id).orFail()) as UserModel;
     const { id, email, orgName, orgType } = user;
     res.status(200).json({ id, email, orgName, orgType });
   } catch (err) {
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,29 +1,13 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import { JwtPayload } from '../models/User';
+import jwt from 'express-jwt';
+import { Request } from 'express';
 
-//Authentication
-export const checkLogIn = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const token = req.headers.authorization?.split(' ')[1] || '';
-    jwt.verify(token, process.env.SECRET_KEY as string, (err, decoded) =>
-      decoded ? next() : next({ status: 401, message: 'Please log in first!' }),
-    );
-  } catch (err) {
-    return next({ status: 401, message: 'Please log in first!' });
-  }
-};
+export interface AuthRequest extends Request {
+  auth?: {
+    id: string;
+  };
+}
 
-//Authorization
-export const checkCorrectUser = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const token = req.headers.authorization?.split(' ')[1] || '';
-    jwt.verify(token, process.env.SECRET_KEY as string, (err, decoded) =>
-      (decoded as JwtPayload)?.id === req.body.id
-        ? next()
-        : next({ status: 401, message: 'Unauthorized!' }),
-    );
-  } catch (err) {
-    return next({ status: 401, message: 'Unauthorized!' });
-  }
-};
+export default jwt({
+  secret: process.env.SECRET_KEY as string,
+  userProperty: 'auth',
+});
