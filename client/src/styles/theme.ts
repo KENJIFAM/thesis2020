4a5692fe6231a7288a5f861836f216fb3ff4a228
Khoa Pham <kenjifam@gmail.com>
2020-03-29 15:38:08 +0300

    change default typo style

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -26,6 +26,30 @@ const theme = createMuiTheme({
   typography: {
     fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
       'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
+    h1: {
+      fontWeight: 400,
+      fontSize: '2.5rem',
+    },
+    h2: {
+      fontWeight: 400,
+      fontSize: '2.25rem',
+    },
+    h3: {
+      fontWeight: 500,
+      fontSize: '2rem',
+    },
+    h4: {
+      fontWeight: 500,
+      fontSize: '1.75rem',
+    },
+    h5: {
+      fontWeight: 500,
+      fontSize: '1.5rem',
+    },
+    h6: {
+      fontWeight: 600,
+      fontSize: '1.25rem',
+    },
   },
 });
 console.log(theme);
