857bff736b5d6eff1ff01277d0e5d3c5b5ec8364
Khoa Pham <kenjifam@gmail.com>
2020-04-02 23:32:24 +0300

    confix socket.io server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import helmet from 'helmet';
+import http from 'http';
+import socketIo from 'socket.io';
 import authController from './controllers/authController';
 import userController from './controllers/userController';
 import requestController from './controllers/requestController';
+import chatController, { socketController } from './controllers/chatController';
 import errorHandler from './middleware/error';
 import auth from './middleware/auth';
 
@@ -19,10 +22,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/auth', authController);
 app.use('/profile/', auth, userController);
 app.use('/requests/', auth, requestController);
+app.use('/chat/', chatController);
 
 app.get('/*', (req, res) => res.send('Welcome to Food Help APIs!'));
 
 app.use(errorHandler);
 
+const server = http.createServer(app);
+const io = socketIo(server, {
+  path: '/chat',
+});
+
+io.on('connection', (socket) => socketController(socket, io));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
